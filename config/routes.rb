45bdc4a63c1693a787a18e36b86faef86b850757
rencewang <47406212+rencewang@users.noreply.github.com>
2024-03-22 22:59:14 -0700

    adding set up

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
+  devise_for :users
+
+  resources :users, only: [:index, :show]
+  resources :posts, only: [:index, :show, :new, :create]
+
+  root to: 'users#index'
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
