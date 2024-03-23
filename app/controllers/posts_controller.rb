a56e0d63430960fc9a6ffd1b96f5eb9b02823c2f
rencewang <47406212+rencewang@users.noreply.github.com>
2024-03-22 23:00:47 -0700

    rubocop

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,27 +1,25 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, except: %i[index show]
 
-    before_action :authenticate_user!, except: [:index, :show]
+  def index
+    @posts = Post.all
+  end
 
-    def index
-        @posts = Post.all
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user = current_user
+    if @post.save
+      redirect_to @post
+    else
+      render :new
     end
-
-    def show
-        @post = Post.find(params[:id])
-    end
-
-    def new
-        @post = Post.new
-    end
-
-    def create
-        @post = Post.new(post_params)
-        @post.user = current_user
-        if @post.save
-            redirect_to @post
-        else
-            render :new
-        end
-    end
-
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,4 @@ class Post < ApplicationRecord
 
   validates :title, presence: true
   validates :body, presence: true
-
-  
 end
