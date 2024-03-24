5c9f1a7c42ca21321009fde76dff5defcb5e95aa
rencewang <47406212+rencewang@users.noreply.github.com>
2024-03-23 23:46:38 -0700

    add username field to post display

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :authenticate_user!, except: %i[index show]
 
   def index
-    @posts = Post.all
+    @posts = Post.includes(:user).all
   end
 
   def show
